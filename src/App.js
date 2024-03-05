97824cb1c101d6fe35d9099ac567271190d755d7
sakshamdubeyyy <109180511+sakshamdubeyyy@users.noreply.github.com>
2024-03-05 09:45:01 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useEffect, useState } from 'react';
+import CountryCard from './CountryCard';
+import axios from 'axios';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [countryName, setCountryName] = useState('')
+
+  const containerStyle = {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh"
+  }
+  
+  const getCountries = async() => {
+    try{
+      let res = await axios.get("https://restcountries.com/v3.1/all");
+      setCountries(res.data);
+    }catch(e){
+      console.error("Cannot fetch countries")
+    }
+  }
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  useEffect(() => {
+    let fC = countries.filter(country => country.name.common.toLowerCase().includes(countryName.toLocaleLowerCase()));
+    let timer = setTimeout(()=>{
+      if(countryName.length >= 1){
+        setCountries(fC);
+      }else{
+        getCountries();
+      }
+    }, 500)
+
+    return (() => {
+      clearTimeout(timer);
+    })
+  }, [countryName]);
+
+  console.log(countryName)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <nav>
+          <input value={countryName} type='text' onChange={e => setCountryName(e.target.value)}/>
+        </nav>
+      </div>
+      <div style={containerStyle}>
+        {countries.map((country) => (
+          <CountryCard country={country} />
+        ))}
+    </div>
     </div>
   );
 }
